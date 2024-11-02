2db5ab82e71b7fba5ad8cf23bbc81307cf570a3f
AlexZ-WP <alexz10@mail.ru>
2024-11-02 22:34:46 +0300

    Add files via upload

--- module_4_2.py
+++ module_4_2.py
@@ -1,18 +1,19 @@
-test_= "Nothing see"
 
+#inner_function("Я в области видимости функции test_function")
 def test_function(test):
     print(test)
-    #global test_
+
     def inner_function(test_):
-        #test_= 3
+
         print(test_)
     inner_function("Я в области видимости функции test_function")
 
     return test
-    inner_function("Nothing see")
-b = test_function("Это  объемлющую область видимости")
-test_= "Nothing see"
 
+
+b = test_function("Это  объемлющую область видимости")
+#test_= "Nothing see"
+#inner_function("Я в области видимости функции test_function")
 
 
 
